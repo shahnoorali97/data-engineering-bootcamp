7cfe20ea817d16fe87f1950391f1db6d5ca9520a
Shahnoor Ali <shahnoorali@Shahnoors-MacBook-Air.local>
2025-10-13 21:47:17 +0300

    Daily aggregation query added

--- fact-data-modeling/user_devices_cumulated.sql
+++ fact-data-modeling/user_devices_cumulated.sql
@@ -5,3 +5,13 @@ CREATE TABLE user_devices_cumulated (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (user_id, browser_type)
 )
+
+WITH daily_activity AS (
+  SELECT
+    user_id,
+    device_id,
+    DATE(event_time) AS activity_date
+  FROM events
+  WHERE user_id IS NOT NULL
+  GROUP BY user_id, device_id, DATE(event_time)
+)
