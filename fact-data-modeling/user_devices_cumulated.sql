c443059a657a3cf3a81d666770af57dd179b2510
Shahnoor Ali <shahnoorali@Shahnoors-MacBook-Air.local>
2025-10-13 22:44:56 +0300

    changed variables of users_devices_cumulated table

--- fact-data-modeling/user_devices_cumulated.sql
+++ fact-data-modeling/user_devices_cumulated.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user_devices_cumulated (
     user_id TEXT NOT NULL,
-    browser_type TEXT NOT NULL,
+    device_id TEXT NOT NULL,
     device_activity_datelist DATE[],
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, browser_type)
-)
+    PRIMARY KEY (user_id, device_id)
+);
 
 WITH daily_activity AS (
   SELECT
